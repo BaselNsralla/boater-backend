12000abcc3b406df24725de14b92e8be9c9ca6e5
Basel <basel.obeid9661@gmail.com>
2018-01-12 03:21:29 -0800

    get full chat data from graphql, this is bad since its loading the whole chat, i need to find a better way to later write the messages under different ids in accednig order, which could be saved in a separater key under the chatId

--- graph/query-entities/chat.js
+++ graph/query-entities/chat.js
@@ -4,8 +4,31 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLNonNull,
-    GraphQLList
+    GraphQLList,
+    GraphQLInt
 } = require ('graphql')
+
+const _MESSAGE = new GraphQLObjectType({
+    name: 'message',
+    fields: {
+        author: {
+            type: GraphQLString,
+            resolve: context => context.author
+        },
+        contentType: {
+            type: GraphQLString,
+            resolve: context => context.contentType
+        },
+        message: {
+            type: GraphQLString,
+            resolve: context => context.message
+        },
+        time_stamp: {
+            type: GraphQLInt,
+            resolve: context => context.time_stamp
+        }
+    }
+})
 
 const _CHAT = new GraphQLObjectType({
     name: 'chat',
@@ -13,8 +36,14 @@ const _CHAT = new GraphQLObjectType({
         users: {
             type: new GraphQLList(GraphQLString),
             resolve: context => Object.values(context.users)
+        },
+        messages: {
+            type: new GraphQLList(_MESSAGE),
+            resolve: context => Object.values(context.messages)
         }
     }
-
 })
+
+
+
 module.exports = _CHAT
